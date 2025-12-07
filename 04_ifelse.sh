14f673362bd523f24c71a07b7e06157f5371b340
vijay <kumar760838@gmail.com>
2025-12-07 16:06:02 +0530

    04_ifelse

--- 04_ifelse.sh
+++ 04_ifelse.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-NUMBER=$1
-# -gt --> greater than
-# -lt --> less than
-# -eq --> equal
-# -ne --> not equal
-if [ $NUMBER -lt 10 ]
+
+echo "Enter a number"
+read number
+
+if [ $number -gt 10 ]
 then
-    echo "Given number $NUMBER is less than 10"
+    echo "Number is greater than 10"
 else
-    echo "Given number $NUMBER is not less than 10"
+    echo "Number is less than or equal to 10"
 fi
